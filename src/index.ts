d46f23046556d97dac2a2d034ab7282bf0bd68aa
talafarael <127351345+talafarael@users.noreply.github.com>
2024-02-07 10:02:12 +0200

    ejs

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 const express=require('express')
 const mongoose =require( "mongoose")
-
+const path = require('path');
 const app = express()
 const PORT = process.env.PORT || 3000
 const authRouter=require('./authRouter')
 app.use(express.json())
 app.use('/auth',authRouter)
 
-
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname,'ejs'));
 
 app.use(express.static('public'));
 const start = async () => {
 	try {
 	
 		await mongoose.connect("mongodb+srv://artemk2504:farashiner@cluster0.1pp5frh.mongodb.net/")
+
+		app.get('/', (req, res) => {
+			res.render('index');
+})
 		app.listen(PORT, () => {
 			console.log("server run"+PORT )
 		})
