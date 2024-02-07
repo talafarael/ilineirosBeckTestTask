1f2be3efe8d80f434f6ff506eae02ffd980685dc
talafarael <127351345+talafarael@users.noreply.github.com>
2024-02-07 23:53:04 +0200

    ff

--- src/authRouter.ts
+++ src/authRouter.ts
@@ -1,8 +1,8 @@
 const Router=require('express')
 const router=new Router
 const controllerLogin=require('./Router/authLogin')
-const upload=require('./middleware/multer')
-const controlleraAuction=require('./Router/authAuction')
+// const upload=require('./middleware/multer')
+// const controlleraAuction=require('./Router/authAuction')
 //authorization
 router.post('/login', controllerLogin.login)
 router.post('/registration',controllerLogin.registration)
@@ -10,7 +10,7 @@ router.post('/resendemail',controllerLogin.resendemail)
 router.post('/registercreate',controllerLogin.registerCreate)
 router.get('/sendemail',controllerLogin.SendEmail)
 //aukction
-router.post('/createauction',upload.single("image"),)
+// router.post('/createauction',upload.single("image"),)
 
 
 
