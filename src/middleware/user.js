a818cffb9ac6a6a632e19a33dbd22d5fa20cbe8c
talafarael <127351345+talafarael@users.noreply.github.com>
2024-02-24 22:54:29 +0200

    Update user.js

--- src/middleware/user.js
+++ src/middleware/user.js
@@ -18,6 +18,6 @@ module.exports = function (req, res, next) {
 		next()
 	} catch (e) {
 		console.log(e)
-		return res.status(403).json({message: "Пользователь не авторизован"})
+		return res.status(403).json({message: "Пользовательааа не авторизован"})
 	}
 }
